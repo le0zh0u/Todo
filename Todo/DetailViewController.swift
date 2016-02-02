55e2ccdad98a5c2b651a541d1f9c4be67a87b1f4
leozhou <492753662@qq.com>
2016-02-03 00:14:11 +0800

    update

---
+++ Todo/DetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewController.swift
+//  Todo
+//
+//  Created by 周椿杰 on 16/2/3.
+//  Copyright © 2016年 周椿杰. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var childButton: UIButton!
+    @IBOutlet weak var phoneButton: UIButton!
+    @IBOutlet weak var shoppingCartButton: UIButton!
+    @IBOutlet weak var travelButton: UIButton!
+    @IBOutlet weak var todoItem: UITextField!
+    @IBOutlet weak var todoDate: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func resetButtons(){
+        childButton.selected = false
+        phoneButton.selected = false
+        shoppingCartButton.selected = false
+        travelButton.selected = false
+    }
+    @IBAction func childTapped(sender: AnyObject) {
+    }
+    @IBAction func phoneTapped(sender: AnyObject) {
+    }
+    @IBAction func shoppingCartTapped(sender: AnyObject) {
+    }
+    @IBAction func travelTapped(sender: AnyObject) {
+    }
+    @IBAction func okTapped(sender: AnyObject) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
